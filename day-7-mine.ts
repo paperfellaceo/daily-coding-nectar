13a9579b1a4f087be43b4c503bee8417a86be5db
Franco <paperfellaceo@gmail.com>
2021-07-15 23:37:01 -0300

    day 7

---
+++ day-7-mine.ts
@@ -0,0 +1,14 @@
+// Given a string and a number of lines k, print the string in zigzag form.
+
+const s = "thisisazigzag"
+const k = 4
+
+class Zigzag {
+	constructor(sentence, k) {
+		this.#n = sentence.length
+		for (let i = 0; i < k.length; i++) {
+			let j = i
+		}
+	}
+}
+// This is not working...
